83ec6372efdb5bd3855674864d9dabf5511194f7
thyago_fontenele <thyago.fontenele@injunior.com.br>
2022-01-19 20:39:17 -0300

    pegando dados da api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-
 import './App.css';
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client';
+
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'https://countries.trevorblades.com'
+});
+
+const LIST_COUNTRIES = gql`
+  {
+    countries {
+      name
+      code
+    }
+  }
+`;
 
 function App() {
+  const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
+  if (loading || error) {
+    return <p>{error ? error.message : 'Loading...'}</p>;
+  }
+  console.log(data);
   return (
     <div className="App">
       
