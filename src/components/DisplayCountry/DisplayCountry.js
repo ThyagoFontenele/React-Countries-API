ef3c5f5429f524dcd76564be29ffe81536e18035
thyago_fontenele <thyago.fontenele@injunior.com.br>
2022-01-19 23:34:13 -0300

    displayCountry css ok

--- src/components/DisplayCountry/DisplayCountry.js
+++ src/components/DisplayCountry/DisplayCountry.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import classes from "./DisplayCountry.module.css";
 export default function DisplayCountry({country}){
-    console.log(country);
     const iso = (country.code).toLowerCase();
     const bandeira = `https://www.bandeirasnacionais.com/data/flags/h80/${iso}.webp`;
     return(
