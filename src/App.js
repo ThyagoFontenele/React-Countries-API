06eb45769c3b98f5871f6fb5436fc1531a2ee34d
thyago_fontenele <thyago.fontenele@injunior.com.br>
2022-01-19 21:20:05 -0300

    pegando imagens dos paises

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
 import {ApolloClient, InMemoryCache, gql, useQuery} from '@apollo/client';
 
 
@@ -12,23 +11,32 @@ const client = new ApolloClient({
 const LIST_COUNTRIES = gql`
   {
     countries {
-      name
       code
+      name
+      capital
+      currency
+      emoji
+      languages {
+        name
+      }
     }
   }
 `;
 
-function App() {
+export default function App() {
   const {data, loading, error} = useQuery(LIST_COUNTRIES, {client});
   if (loading || error) {
     return <p>{error ? error.message : 'Loading...'}</p>;
   }
-  console.log(data);
+  const iso = (data.countries[0].code).toLowerCase();
+  const bandeira = `https://www.bandeirasnacionais.com/data/flags/h80/${iso}.webp`;
+  console.log(data.countries[2].name);
   return (
     <div className="App">
+
       
+      <img src={bandeira} alt="pais"/>
     </div>
   );
 }
 
-export default App;
