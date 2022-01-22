8f5260abb6c05fcaf0455da67b9a82427fcf57cf
thyago_fontenele <thyago.fontenele@injunior.com.br>
2022-01-22 18:11:51 -0300

    ShowCountry com estilo pronto e responsivo

--- src/components/ShowCountry/ShowCountry.js
+++ src/components/ShowCountry/ShowCountry.js
@@ -1,28 +1,33 @@
 import React, {useState, useEffect} from 'react';
 import classes from "./ShowCountry.module.css";
+import PngBack from '../../assets/pngwing.com.png';
+import { Link } from 'react-router-dom';
 export default function ShowCountry({country}){
     const iso = (country.code).toLowerCase();
     const bandeira = `https://www.bandeirasnacionais.com/data/flags/h80/${iso}.webp`;
     const [idiomaText, setIdiomaText ] = useState('');
 
     useEffect(() => {
-        if(country.languages > 1){
+        if(country.languages.length > 1){
             setIdiomaText('Idiomas');
         }else{ setIdiomaText('Idioma');}
-    }, [])
-    
+        
+    })
+
     return(
         <>
-            <div>
-                <img src={bandeira} alt={`bandeira ${country.name}`}/>
-                <h1>Nome: {country.name}</h1>
-                <p>Capital: {country.capital}</p>
-                <p>Sigla: {country.emoji}</p>
-                <p>{idiomaText}: {country.languages.map(ele => ele.name+" ")}</p>
-                <p>Moeda: {country.currency}</p>
-                
+            <Link to="/" ><img src={PngBack} alt="voltar" className={classes.back}/></Link> 
+            <div className={classes.body}>
+                <div className={classes.container}>
+                    <h1>{country.name}</h1>
+                    <img src={bandeira} alt={`bandeira ${country.name}`}/>
+                    <p><span>Capital:</span> {country.capital}</p>
+                    <p><span>Continente:</span> {country.continent.name}</p>
+                    <p><span>Emoji:</span> {country.emoji}</p>
+                    <p><span>{idiomaText}:</span> {country.languages.map(ele => ele.name+" | ")}</p>
+                    <p><span>Moeda:</span> {country.currency}</p>
+                </div>
             </div>
-           
         </>
     )
 }
